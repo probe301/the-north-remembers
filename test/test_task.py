a033a8a34386f04971fd42f6ecd32072d7c2d416
Probe <probe301@gmail.com>
2019-12-08 19:25:26 +0800

    add test for task and watcher

--- test/test_task.py
+++ test/test_task.py
@@ -11,7 +11,7 @@ from pydantic import (HttpUrl, DirectoryPath, FilePath,)
 import dictdiffer
 import pytest
 from task import Task
-
+import time
 DESC = {
   'url': 'https://zhuanlan.zhihu.com/p/67815990', 
   'tip': 'test task', 
@@ -151,11 +151,69 @@ def test_3_task_default_params():
   task.max_cycle | should.equal('3days')
 
 
-def test_4_task_to_yaml_text(): pass
-def test_5_task_priority():  pass
-def test_6_task_fethcer_option(): 
+def test_4_task_to_yaml_text(): 
   desc = {
     'url': 'https://zhuanlan.zhihu.com/p/67815990', 
   }
+  task = Task.create(desc, env_option=ENV_OPTION, fetcher_option=FETCHER_OPTION)
+  yaml_should = f'''- url: https://zhuanlan.zhihu.com/p/67815990
+  tip: "Default Tip"
+  timestamp: {{ task_add: "{tools.time_now_str()}", last_watch: "None", last_change: "None", next_watch: "{tools.time_now_str()}" }}
+  version: 0'''
+  task.to_yaml_text() | should.equal(yaml_should)
+
+
+def test_5_task_priority(): 
+  ''' 优先级受 weight task_add_time next_watch_time last_watch_time 影响
+      weight 数值应该 >= 0, next_watch_time 应该已经落后于当前时间, 差距秒数称为 delta
+      weight > 1, 直接以 weight 作为优先级, 忽略 last_watch_time next_watch_time
+        优先级相同时, task_add_time 最晚的优先级高
+        (weight 设为 > 1 是特殊情况, 表示希望立即抓取最近添加的任务)
+      weight <= 1, 以 weight 作为基础的优先级, 以其他条件做调整
+        - next_watch_time 越早先的, 优先级越大, 具体为在 weight 上减去 1/(next_watch_delta + 100)
+        - 已经有了 last_watch_time, 则将最终的优先级 *=0.8
+  '''
+  desc1 = {'url': 'https://zhuanlan.zhihu.com/p/67815990'}
+  task1 = Task.create(desc1, env_option=ENV_OPTION, fetcher_option=FETCHER_OPTION)
+  desc2 = {'url': 'https://zhuanlan.zhihu.com/frontEndInDepth'}
+  task2 = Task.create(desc2, env_option=ENV_OPTION, fetcher_option=FETCHER_OPTION)
+  assert task1.priority == task2.priority
+  
+  desc1 = {'url': 'https://zhuanlan.zhihu.com/p/67815990', 'weight': 3}
+  task1 = Task.create(desc1, env_option=ENV_OPTION, fetcher_option=FETCHER_OPTION)
+  desc2 = {'url': 'https://zhuanlan.zhihu.com/frontEndInDepth', 'weight': 2}
+  task2 = Task.create(desc2, env_option=ENV_OPTION, fetcher_option=FETCHER_OPTION)
+  time.sleep(1)
+  desc3 = {'url': 'https://zhuanlan.zhihu.com/frontEndInDepth', 'weight': 2}
+  task3 = Task.create(desc3, env_option=ENV_OPTION, fetcher_option=FETCHER_OPTION)
+  desc4 = {'url': 'https://zhuanlan.zhihu.com/frontEndInDepth', 'weight': 3}
+  task4 = Task.create(desc4, env_option=ENV_OPTION, fetcher_option=FETCHER_OPTION)
+  assert task4.priority > task1.priority > task3.priority > task2.priority
+
+
+
+def test_6_task_fethcer_option(): 
+  desc = { 'url': 'https://zhuanlan.zhihu.com/p/67815990', }
   task = Task.create(desc, env_option={}, fetcher_option={})
   task.fetcher_option.limit | should.equal(300)
+  task = Task.create(desc, env_option={}, fetcher_option={'limit': 200})
+  task.fetcher_option.limit | should.equal(200)
+  task = Task.create(desc, env_option={'limit': 200}, fetcher_option={})
+  task.fetcher_option.limit | should.equal(300)  # key limit should in fetcher_option
+
+  task = Task.create(desc, env_option={'page_min_cycle': '31minutes', 'lister_min_cycle': '32minutes'}, fetcher_option={})
+  task.min_cycle | should.equal('31minutes')
+
+
+def test_7_task_dispatch():
+  desc1 = {'url': 'https://zhuanlan.zhihu.com/p/67815990'}
+  task1 = Task.create(desc1, env_option=ENV_OPTION, fetcher_option=FETCHER_OPTION)
+  desc2 = {'url': 'https://zhuanlan.zhihu.com/frontEndInDepth'}
+  task2 = Task.create(desc2, env_option=ENV_OPTION, fetcher_option=FETCHER_OPTION)
+  from task import PageTask, ListerTask
+  assert type(task1) == PageTask
+  assert type(task2) == ListerTask
+
+
+def test_8_task_run():
+  pass
--- test/test_watcher.py
+++ test/test_watcher.py
@@ -1,6 +1,6 @@
 
-import os, sys 
-parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+import os, sys
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, parentdir)
 import shutil
 import tools
@@ -8,12 +8,24 @@ from watcher import Watcher
 from pyshould import should
 import arrow
 from datetime import datetime
+from time import sleep
 import dictdiffer
 import pytest
 from task import Task
 
+'''
+#### Watcher
+任务调度, 监视文件夹, 
+.config.yaml 抓取页面
+.tasks.yaml 记录历史
 
+task_type: page, lister, 
 
+通过扫描已有的待抓取任务, 在手动触发或者达到特定时间后, 分派抓取任务
+
+'''
+
+ROOTPATH = r'D:/DataStore'
 PROJECTPATH = r'D:/DataStore/Test Watcher'
 PROJECTPATHOPEN = r'D:/DataStore/Test Watcher Open'
 PROJECTPATHNEW = r'D:/DataStore/Test Watcher New'
@@ -41,7 +53,7 @@ rss_output_path: 'feed.xml'
 epub_output_path: none
 # Lister Task and Page Task
 urls:
-  - url: https://zhuanlan.zhihu.com/frontEndInDepth       
+  - url: https://zhuanlan.zhihu.com/frontEndInDepth
     tip: "专栏 TryFEInDepth"
     option: {limit: 4}
   - url: https://zhuanlan.zhihu.com/learn-vue-source-code
@@ -52,34 +64,37 @@ urls:
       text_exclude: '有哪些,如何看待'  # 标题/话题中有这些关键词, 就不抓取limit: 10
 '''
 
+PROJECTPATHWITHGIT = PROJECTPATH + ' withgit'
+CONFIGDATAWITHGIT = CONFIGDATA.replace("git_commit_path: ''", "git_commit_path: '.'")
 
 
 @pytest.fixture()  # default scope='function'
 def setup_git_project(request):
   def teardown_function():
     print("teardown_git_project...")
-    shutil.rmtree(PROJECTPATH, ignore_errors=True)
-  request.addfinalizer(teardown_function)  
+    # tools.run_command(f'cd "{ROOTPATH}"')
+    # sleep(3)
+    # shutil.rmtree(PROJECTPATHWITHGIT, ignore_errors=False)
+  request.addfinalizer(teardown_function)
 
   print('setup_git_project...')
-  shutil.rmtree(PROJECTPATH, ignore_errors=True)
-  os.makedirs(PROJECTPATH, exist_ok=True)
-  configdata = CONFIGDATA.replace("git_commit_path: ''", "git_commit_path: '.'")
-  tools.save_txt(PROJECTPATH + '/.config.yaml', configdata)
-  cmd = f'cd "{PROJECTPATH}" && git init'
+  shutil.rmtree(PROJECTPATHWITHGIT, ignore_errors=True)
+  os.makedirs(PROJECTPATHWITHGIT, exist_ok=True)
+  tools.text_save(PROJECTPATHWITHGIT + '/.config.yaml', CONFIGDATAWITHGIT)
+  cmd = f'cd "{PROJECTPATHWITHGIT}" && git init'
   tools.run_command(cmd)
-  cmd = f'cd "{PROJECTPATH}" && git config user.name TNR && git config user.email tnr@email.com'
+  cmd = f'cd "{PROJECTPATHWITHGIT}" && git config user.name TNR && git config user.email tnr@email.com'
   tools.run_command(cmd)
-  cmd = f'cd "{PROJECTPATH}" && git add . && git commit -m "init TNR project"'
+  cmd = f'cd "{PROJECTPATHWITHGIT}" && git add . && git commit -m "init TNR project"'
   tools.run_command(cmd)
 
 
 def test_1_watcher_open():
   os.makedirs(PROJECTPATHOPEN, exist_ok=True)
-  tools.save_txt(PROJECTPATHOPEN + '/.config.yaml', CONFIGDATA)
+  tools.text_save(PROJECTPATHOPEN + '/.config.yaml', CONFIGDATA)
   w = Watcher.open(PROJECTPATHOPEN)
   w.report()
-  shutil.rmtree(PROJECTPATHOPEN, ignore_errors=True)
+  shutil.rmtree(PROJECTPATHOPEN, ignore_errors=False)
 
 
 def test_2_watcher_create():
@@ -87,16 +102,17 @@ def test_2_watcher_create():
   Watcher.create(PROJECTPATHNEW)
   w = Watcher.open(PROJECTPATHNEW)
   w.report()
-  shutil.rmtree(PROJECTPATHNEW, ignore_errors=True)
+  shutil.rmtree(PROJECTPATHNEW, ignore_errors=False)
 
 def test_3_git_report(setup_git_project):
-  w = Watcher.open(PROJECTPATH)
-  w.folder  | should.equal('Test Watcher')
-  w.listers | should.equal(['https://zhuanlan.zhihu.com/frontEndInDepth', 'https://zhuanlan.zhihu.com/learn-vue-source-code'])
+  w = Watcher.open(PROJECTPATHWITHGIT)
+  w.folder | should.equal('Test Watcher withgit')
+  lister_urls = [t.url for t in w.get_lister_tasks()]
+  lister_urls | should.equal(['https://zhuanlan.zhihu.com/frontEndInDepth', 'https://zhuanlan.zhihu.com/learn-vue-source-code'])
   w.report()
 
 def test_4_watch_once(setup_git_project):
-  w = Watcher.open(PROJECTPATH)
+  w = Watcher.open(PROJECTPATHWITHGIT)
   w.report()
   w.watch_once()
   for path in tools.all_files(w.watcher_path, '*.md', ):

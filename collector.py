3e92a66d2f5c6e7884c5a379b1feb73e77c939a7
Probe <probe301@gmail.com>
2020-01-03 18:02:47 +0800

    edit col

--- collector.py
+++ collector.py
@@ -6,7 +6,6 @@ import re
 import random
 from datetime import datetime
 
-from watcher import Watcher
 from fetcher import Fetcher
 from collections import OrderedDict as odict
 from collections import Counter
@@ -16,7 +15,7 @@ import tools
 log = tools.create_logger(__file__)
 log_error = tools.create_logger(__file__ + '.error')
 
-
+from task import Task
 from fetcher import UrlType
 from fetcher import parse_type
 from fetcher import purge_url
@@ -29,26 +28,12 @@ from tools import time_from_str
 from tools import time_to_humanize
 from tools import remove_invalid_char
 
+from fetcher import Fetcher
 from page import Page
 # from werkzeug.contrib.atom import AtomFeed
 from feedgen.feed import FeedGenerator
 
 
-
-# 创建 Watcher 目录时的默认 lister task 和 page task 设置
-LISTER_DEFAULT_OPTION = odict(
-  enabled=True,
-  max_cycle='30days',
-  min_cycle='12hours',
-  weight=0.5,
-  limit=200,
-)
-PAGE_DEFAULT_OPTION = odict(
-  enabled=True,
-  max_cycle='180days',
-  min_cycle='45days',
-  weight=0.5,
-)
 
 class Collector:
   '''
@@ -74,13 +59,9 @@ class Collector:
 
 
 
-  # 向已有的 Watcher 更新条目
-  TODO collector.append_to_watcher(use watcher.add_task(task_dict))
-
-
+  # TODO 向已有的 Watcher 更新条目
 
   '''
-
 
   def __init__(self, project_path):
     ''' 创建 Collector 
@@ -88,34 +69,55 @@ class Collector:
         同时检测项目路径是否为 git 仓库 TODO
 
         结构是 project_path (git repo)
-                |
+                |-- .config.yaml
+                |-- .tasks.yaml
                 |-- watcher_path1
-                |     |-- .task.yaml
+                |     |-- saved_page1.md
+                |     |-- saved_page2.md
                 |-- watcher_path2
-                |     |-- .task.yaml
+                |     |-- saved_page3.md
                 |-- watcher_path3
                 | ..........
     '''
     if not os.path.exists(project_path):
       raise FileNotFoundError(f'project `{project_path}` does not exists')
-    tools.run_command(f'cd "{project_path}" && git status') # check git repo
+    if not os.path.exists(project_path + '/.config.yaml'):
+      raise FileNotFoundError(f'project `{project_path}` .config.yaml does not exists')
+
+    # tools.run_command(f'cd "{project_path}" && git status') # check git repo
+    config = tools.yaml_load(project_path + '/.config.yaml')
     self.project_path = project_path
-    
+
+    # 合并来自.config.yaml的设置项, 以及默认设置项
+    # self.project_option = tools.dict_merge(PROJECT_DEFAULT_OPTION, config.get('project_option', {})) 
+    # self.default_task_option = tools.dict_merge(TASK_DEFAULT_OPTION, config.get('default_task_option', {})) 
+    self.watchers = []
+    for folder, config_data in config['watchers'].items():
+      watcher_path = os.path.join(project_path, folder)
+      os.makedirs(watcher_path, exist_ok=True)
+      # watcher = Watcher(watcher_path, config_data, self.default_task_option)
+      self.watchers.append(watcher)
 
   def __str__(self):
-    return f'<Collector #{id(self)} path=`{self.project_path}`>'
+    return f'<Collector #{id(self)} path=`{self.project_path}` watchers={self.watcher_count}>'
+
+  @property
+  def watcher_count(self):
+    return len(self.watchers)
 
 
   @classmethod
-  def create_project(cls, path):
+  def create(cls, path, config):
     ''' 创建项目或打开已存在项目
         新建时附带生成 git 仓库'''
     if os.path.exists(path):
-      log(f'WARN: create_project `{path}` exists')
-    else:
-      os.mkdir(path)
+      raise FileExistsError(f'WARN: create_project `{path}` exists')
+
+    os.mkdir(path)
+    tools.yaml_save(config, path + '/.config.yaml')
+    if config.get('project_option', {}).get('version_control') == 'git':
       ignore = "# project\n.ipynb_checkpoints\n__pycache__/\n*.py[cod]\ntodo.txt\n**/feed.xml"
-      tools.save_txt(os.path.join(path, '.gitignore'), ignore)
+      tools.text_save(os.path.join(path, '.gitignore'), ignore)
       cmd = f'cd "{path}" && git init'
       tools.run_command(cmd)
       cmd = f'cd "{path}" && git config user.name TNR && git config user.email tnr@email.com'
@@ -123,212 +125,170 @@ class Collector:
       cmd = f'cd "{path}" && git add . && git commit -m "init TNR project"'
       tools.run_command(cmd)
       log(f'create new project {path} and committed')
+    else:
+      log(f'create new project {path}')
     return cls(project_path=path)
 
-  def report(self):
-    ''' 输出每个 Watcher folder 的摘要 '''
-    log(f'report {self} \n---------------------')
-    for directory in self.iter_watcher_paths():
-      folder = os.path.basename(directory)
-      folder_md5 = tools.md5(folder, 10)
-      all_pages = tools.all_files(directory, patterns='*.md', single_level=True)
-      count = len(list(all_pages))
-      log(f'    Watcher: "{folder}" (or "{folder_md5}") ({count} pages)')
-    output = tools.run_command(f'cd "{self.project_path}" && git log --oneline -n 5')
-    log('git log: ')
-    for line in output.splitlines():
-      log(f'    {line.strip()}')
-    log(f'---------------------')
-
-
-  def iter_watcher_paths(self):
-    for directory in tools.all_subdirs(self.project_path):
-      task_yaml = os.path.join(directory, '.task.yaml')
-      if os.path.exists(task_yaml):
-        yield directory
-
-
-  def create_watcher(self, urls, tips=None, folder=None, parent_folder='.', lister_option=None, page_option=None):
-    ''' 添加一个 Watcher, 根据 urls 创建 folder, 
-        urls: 一个或多个网址, 多个需属于同种类型, 目前未检测 
-        tips: 与 urls 配套的描述字符串
-        folder: 指定 Watcher 文件夹名, 不指定就自动生成
-    '''
-    if isinstance(urls, str): urls = [urls, ]
-    if isinstance(tips, str): tips = [tips, ]
-    urls = [purge_url(u) for u in urls]
-    if tips is None: tips = [Fetcher.generate_tip(u) for u in urls]
-    if len(urls) != len(tips):
-      raise ValueError(f'count of {urls} should equal to count {tips}')
-
-
-    if folder is None:
-      folder = Fetcher.generate_folder_name(urls)
-      folder = tools.remove_invalid_char(folder)  # 只有自动生成 folder 需要处理 invalid_char
-    watcher_path = os.path.join(self.project_path, parent_folder, folder)
-    if os.path.exists(watcher_path):
-      raise FileExistsError(f'creating watcher, already exists: `{watcher_path}`')
-
-    d = odict(
-        default_option=odict(
-            lister=tools.dict_merge(LISTER_DEFAULT_OPTION, lister_option),
-            page=tools.dict_merge(PAGE_DEFAULT_OPTION, page_option),
-        ), 
-        lister_task=[odict(url=url, tip=tip) for url, tip in zip(urls, tips)]
-    )
-    # 这样会让 lister_task[] 的缩进不正确
-    # 应该是 lister_task:
-    #          - url: xxx
-    #            tip: xxx
-    # 实际是 lister_task:
-    #        - url: xxx
-    #          tip: xxx 但是不影响识别
-
-    os.makedirs(watcher_path)
-    tools.yaml_save(d, watcher_path + '/' + '.task.yaml')
-    log(f'Watcher path {watcher_path} created')
-    folder_name = os.path.basename(watcher_path)
-    self.remember(f'create Watcher folder {folder_name}')
-
-
-  def create_multiple_watchers(self, config):
-    ''' 依据 config 创建多个 watcher folder
-    config 样例 (format as yaml): 
-    
-        global_default_option: 
-          lister: 
-            max_cycle: 30days
-            min_cycle: 12hours
-            limit: 200
-          page: 
-            weight: 0.5
-
-        watcher_config:
-          "folder/subfolder":
-            default_option:
-              lister:
-                min_cycle: 18hours
-                limit: 300
-              page:
-                min_cycle: 45days
-                weight: 0.8
-            lister_task:
-                - 'https://zhuanlan.zhihu.com/xxxx #注释'
-                - 'https://zhuanlan.zhihu.com/yyyy #注释'
-                - url: https://zhuanlan.zhihu.com/zzzz
-                  tip: "知乎专栏 - zzzz"
-                  zhihu_min_voteup: 100
-          "folder/subfolder2":
-              lister_task:
-                - 'https://url   #注释'
-                - 'https://url   #注释'
-                - 'https://url   #注释'
-
-    对 config 任务的说明:
-      都要位于 watcher_config 之下的 "folder/subfolder" 中
-      该 "folder/subfolder" 将自动创建, 
-      "folder/subfolder" 内部的配置与 Watcher 的 '.task.yaml' 一致, 
-      特殊的, 如果 lister_task 列表中的条目是字符串 (正常应该是 {'url': xxx, 'tip': yyy})
-      视为 {'url': 从该字符串中提取 url 部分}
-    对 config 参数配置的说明:
-      参数优先级从低到高依次是:
-      1 "global_default_option:" 中的 lister: page:
-      2 "watcher_config:" 中, 对每个 folder 独立配置的 "default_option:"
-      3 每个 lister task 独立配置的选项
-      更具体的选项配置将覆盖全局的选项配置
-    '''
-
-    log(f'create_multiple_watchers... ')
-    # pprint(config)
-    global_default_option = config.get('global_default_option', {'lister': {}, 'page': {}})
-    global_default_lister_option = global_default_option.get('lister', {})
-    global_default_page_option = global_default_option.get('page', {})
-
-    for path, task_config in config.get('watcher_config', []).items():
-      # pprint(path)
-      # pprint(task_config)
-      folder = os.path.basename(path)
-      parent_folder = os.path.dirname(path)
-      folder_default_option = task_config.get('default_option', {})
-      lister_option = tools.dict_merge(global_default_lister_option, folder_default_option.get('lister', {}))
-      page_option = tools.dict_merge(global_default_page_option, folder_default_option.get('page', {}))
-      urls = [url.split(' ')[0] for url in task_config.get('lister_task', [])]
-
-      # print('urls', urls, 'folder', folder, 'parent_folder', repr(parent_folder), )
-      # print('lister_option:' ), pprint(lister_option, )
-      # print('page_option:'), pprint(page_option, )
-
-      self.create_watcher(urls, tips=None, 
-                          folder=folder, parent_folder=parent_folder, 
-                          lister_option=lister_option, page_option=page_option)
-    log(f'create_multiple_watchers... done')
-
-
-
-  def find_watcher_path(self, folder_name, strict=True):
-    ''' strict=False 时, 查找 folder_name 近似, 或者 md5 吻合的 watcher_path'''
-    if strict:
-      for directory in self.iter_watcher_paths():
-        if folder_name == os.path.basename(directory):
-          return directory
-      return None
-    else:  # strict False 时
-      clean = lambda text: tools.remove_invalid_char(text).replace(' ', '')
-      for directory in self.iter_watcher_paths():
-        if clean(folder_name) == clean(os.path.basename(directory)):
-          return directory
-        if folder_name == tools.md5(os.path.basename(directory), 10):
-          return directory
-      return None
-
   @classmethod
-  def explore(cls, urls):
-    '''分析多个页面的摘要特征'''
-    for url in urls:
-      url = purge_url(url.split(' ')[0])
-      desc = Fetcher.create({'url': url}).detect()
-      log(desc)
-
-
-  def watching(self, folder_name):
-    watcher_path = self.find_watcher_path(folder_name, strict=True)
-    watcher = Watcher(watcher_path)
-    log(f'\n  ↓ start_watching_once for\n  {watcher}')
-    for commit_log in watcher.watch():
-      self.remember(commit_log)
-    log(f'  ↑ start_watching_once done\n')
-
-  def start_watching_loop(self, folder_name):
-    pass
-
-
-  def remember(self, commit_log, verbose=False):
-    ''' 将 watcher 抓取到的内容存储到 git 仓库
-        git 仓库通常位于 watcher folder 的上一层
-    '''
-    # git_path = os.path.dirname(watcher_path) # watcher folder 上一层
-    if isinstance(commit_log, dict):
-      commit_log = commit_log.get('commit_log', 'missing commit log')
-    cmd = f'cd "{self.project_path}" && git add . && git commit -m "{commit_log}"'
-    # log(cmd)
-    tools.run_command(cmd, verbose=verbose)
-    log(f'Watcher.remember committed: "{commit_log}"\n')
+  def open(cls, path):
+    ''' 打开已存在项目 '''
+    if not os.path.exists(path):
+      raise FileNotFoundError(f'project `{path}` not exists')
+    return cls(project_path=path)
 
 
 
-  def generate_feed(self, path, limit, site):
-    ''' 生成 RSS, 内容为全部 page_task, 
+  # def create_watcher(self, urls, tips=None, folder=None, lister_option=None, page_option=None):
+  #   ''' 添加一个 Watcher, 根据 urls 创建 folder, 
+  #       urls: 一个或多个网址, 多个需属于同种类型, 目前未检测 
+  #       tips: 与 urls 配套的描述字符串
+  #       folder: 指定 Watcher 文件夹名, 不指定就自动生成
+  #   '''
+  #   if isinstance(urls, str): urls = [urls, ]
+  #   if isinstance(tips, str): tips = [tips, ]
+  #   urls = [purge_url(u) for u in urls]
+  #   if tips is None: tips = [Fetcher.generate_tip(u) for u in urls]
+  #   if len(urls) != len(tips):
+  #     raise ValueError(f'count of {urls} should equal to count {tips}')
+
+
+  #   if folder is None:
+  #     folder = Fetcher.generate_folder_name(urls)
+  #     folder = tools.remove_invalid_char(folder)  # 只有自动生成 folder 需要处理 invalid_char
+  #   watcher_path = os.path.join(self.project_path, folder)
+  #   if os.path.exists(watcher_path):
+  #     raise FileExistsError(f'creating watcher, already exists: `{watcher_path}`')
+
+  #   d = odict(
+  #       default_option=odict(
+  #           lister=tools.dict_merge(LISTER_DEFAULT_OPTION, lister_option),
+  #           page=tools.dict_merge(PAGE_DEFAULT_OPTION, page_option),
+  #       ), 
+  #       lister_task=[odict(url=url, tip=tip) for url, tip in zip(urls, tips)]
+  #   )
+  #   # 这样会让 lister_task[] 的缩进不正确
+  #   # 应该是 lister_task:
+  #   #          - url: xxx
+  #   #            tip: xxx
+  #   # 实际是 lister_task:
+  #   #        - url: xxx
+  #   #          tip: xxx 但是不影响识别
+
+  #   os.makedirs(watcher_path)
+  #   tools.yaml_save(d, watcher_path + '/' + '.task.yaml')
+  #   log(f'Watcher path {watcher_path} created')
+  #   folder_name = os.path.basename(watcher_path)
+  #   self.remember(f'create Watcher folder {folder_name}')
+
+
+  # def create_multiple_watchers(self, config):
+  #   ''' 依据 config 创建多个 watcher folder
+  #   config 样例 (format as yaml): 
+    
+  #       global_default_option: 
+  #         lister: 
+  #           max_cycle: 30days
+  #           min_cycle: 12hours
+  #           limit: 200
+  #         page: 
+  #           weight: 0.5
+
+  #       watcher_config:
+  #         "folder/subfolder":
+  #           default_option:
+  #             lister:
+  #               min_cycle: 18hours
+  #               limit: 300
+  #             page:
+  #               min_cycle: 45days
+  #               weight: 0.8
+  #           lister_task:
+  #               - 'https://zhuanlan.zhihu.com/xxxx #注释'
+  #               - 'https://zhuanlan.zhihu.com/yyyy #注释'
+  #               - url: https://zhuanlan.zhihu.com/zzzz
+  #                 tip: "知乎专栏 - zzzz"
+  #                 min_voteup: 100
+  #         "folder/subfolder2":
+  #             lister_task:
+  #               - 'https://url   #注释'
+  #               - 'https://url   #注释'
+  #               - 'https://url   #注释'
+
+  #   对 config 任务的说明:
+  #     都要位于 watcher_config 之下的 "folder/subfolder" 中
+  #     该 "folder/subfolder" 将自动创建, 
+  #     "folder/subfolder" 内部的配置与 Watcher 的 '.task.yaml' 一致, 
+  #     特殊的, 如果 lister_task 列表中的条目是字符串 (正常应该是 {'url': xxx, 'tip': yyy})
+  #     视为 {'url': 从该字符串中提取 url 部分}
+  #   对 config 参数配置的说明:
+  #     参数优先级从低到高依次是:
+  #     1 "global_default_option:" 中的 lister: page:
+  #     2 "watcher_config:" 中, 对每个 folder 独立配置的 "default_option:"
+  #     3 每个 lister task 独立配置的选项
+  #     更具体的选项配置将覆盖全局的选项配置
+  #   '''
+
+  #   log(f'create_multiple_watchers... ')
+  #   # pprint(config)
+  #   global_default_option = config.get('global_default_option', {'lister': {}, 'page': {}})
+  #   global_default_lister_option = global_default_option.get('lister', {})
+  #   global_default_page_option = global_default_option.get('page', {})
+
+  #   for path, task_config in config.get('watcher_config', []).items():
+  #     # pprint(path)
+  #     # pprint(task_config)
+  #     folder = os.path.basename(path)
+  #     parent_folder = os.path.dirname(path)
+  #     folder_default_option = task_config.get('default_option', {})
+  #     lister_option = tools.dict_merge(global_default_lister_option, folder_default_option.get('lister', {}))
+  #     page_option = tools.dict_merge(global_default_page_option, folder_default_option.get('page', {}))
+  #     urls = [url.split(' ')[0] for url in task_config.get('lister_task', [])]
+
+  #     # print('urls', urls, 'folder', folder, 'parent_folder', repr(parent_folder), )
+  #     # print('lister_option:' ), pprint(lister_option, )
+  #     # print('page_option:'), pprint(page_option, )
+
+  #     self.create_watcher(urls, tips=None, 
+  #                         folder=folder, parent_folder=parent_folder, 
+  #                         lister_option=lister_option, page_option=page_option)
+  #   log(f'create_multiple_watchers... done')
+
+
+
+  def find_watcher_path(self, folder_name):
+    ''' 查找 folder_name 近似, 或者 md5 吻合的 watcher_path'''
+    clean = lambda text: tools.remove_invalid_char(text).replace(' ', '')
+    for watcher in self.watchers:
+      if clean(folder_name) == clean(watcher.folder):
+        return watcher.folder
+      if folder_name == tools.md5(watcher.folder, 10):
+        return watcher.folder
+    return None
+
+  # @classmethod
+  # def explore(cls, urls):
+  #   '''分析多个页面的摘要特征'''
+  #   for url in urls:
+  #     url = purge_url(url.split(' ')[0])
+  #     desc = Fetcher.create({'url': url}).detect()
+  #     log(desc)
+
+
+
+  def to_rss_feed(self, folder, limit=150, order='edit_date', site=''):
+    ''' 生成 RSS, 内容为全部 pages, 
         按照添加任务的顺序倒序排列 (按照更新时间排列? TODO)
-        TODO path 如果是父级文件夹, 视为合并子文件夹里所有 page 生成 rss feed
         RSS Feed 文件名为 path, alias? TODO
-
         limit=-1 时迭代所有Page
-        '''
-    watcher_path = self.find_watcher_path(path, strict=False)
+        order=edit_date|create_date|title
+        TODO: 指定输出 xml 文件位置
+        TODO: rss 订阅源的设置 site link, 等
+    '''
+    watcher_path = self.find_watcher_path(folder)
     if not watcher_path: 
-      log(f'WARN: generate_feed path {path} not found')
+      log(f'WARN: generate_feed path {watcher_path} not found')
       return None
-    feed_name = os.path.basename(watcher_path)  # TODO 如果是父级文件夹, 合并
+    feed_name = os.path.basename(watcher_path)
     pages = []
     for path in tools.all_files(watcher_path, patterns='*.md'):
       if limit == 0: break
@@ -364,45 +324,12 @@ class Collector:
     # log(f'generate_feed `{feed_path}` done')
     return feed_path
 
+  def to_epub(self, path):
+    pass
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# =========================================================
+# =================== end of class Collector ==============
+# =========================================================
 
 
 
